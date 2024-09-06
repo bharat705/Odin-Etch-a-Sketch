464a4aad3269e0b3e095f08bb0479cdd2e852e02
bharat705 <73097406+bharat705@users.noreply.github.com>
2024-09-06 09:54:42 +0545

    Add range input and border toggle
    - Implement range input for grid size (1 to 100)
    - Add toggle button to show/hide grid borders
    Changes to be committed:
    	modified:   index.html
    	modified:   scripts.js
    	modified:   styles.css

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
+let isGridOn = false;
+
 const gridContainer = document.querySelector("#gridContainer");
 const inputGridNumber = document.querySelector("#inputGridNumber");
+const rangeSliderValue = document.querySelector(".rangeSliderValue");
 
 const containerSize = 960;
 const gridNumber = Number(inputGridNumber.value);
@@ -7,7 +10,7 @@ gridContainer.style.width = containerSize + "px";
 gridContainer.style.height = containerSize + "px";
 
 function createGrids(num) {
-  const divSize = containerSize / num - 2;
+  const divSize = containerSize / num;
   for (let i = 0; i < num * num; i++) {
     let div = document.createElement("div");
     div.classList.add("grids");
@@ -18,12 +21,37 @@ function createGrids(num) {
 }
 
 function resetGrids() {
-  gridContainer.textContent = ""
+  gridContainer.textContent = "";
 }
 
 inputGridNumber.addEventListener("input", () => {
+  rangeSliderValue.textContent = inputGridNumber.value;
   resetGrids();
   const gridNumber = Number(inputGridNumber.value);
   createGrids(gridNumber);
 });
+
+function toggleBorder(color = "#dcdcdc") {
+  if (isGridOn === true) {
+    for (const child of gridContainer.children) {
+      child.style.border = "1px solid " + color;
+    }
+  } else {
+    for (const child of gridContainer.children) {
+      child.style.border = "none";
+    }
+  }
+}
+
 createGrids(gridNumber);
+
+const gridBorder = document.querySelector(".gridBorder");
+gridBorder.addEventListener("mousedown", () => {
+  if (isGridOn === false) {
+    isGridOn = true;
+    toggleBorder();
+  } else if (isGridOn === true) {
+    isGridOn = false;
+    toggleBorder();
+  }
+});
